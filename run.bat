cbb5d4df6741a48dd146775819ac5984f239b2ae
James Lim <jameslimbj@gmail.com>
2021-07-06 16:02:53 +0800

    fix(version): update global version file

--- run.bat
+++ run.bat
@@ -2,9 +2,13 @@
 git stash
 git fetch --all
 git reset --hard origin/main
+
 java -Xmx4096M -Xms4096M -jar server.jar nogui
+
 set /p Build=<ver.txt
 set /a Build=%Build%+1
+echo %Build%>ver.txt
+
 git add .
 git commit -m "session #%Build%"
 git push
