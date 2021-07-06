e9526bee986e9d108c63da256d7427e547e07d70
James Lim <jameslimbj@gmail.com>
2021-07-06 21:16:30 +0800

    fix(version): rewrite syntax for storing version

--- run.bat
+++ run.bat
@@ -7,7 +7,7 @@ java -Xmx4096M -Xms4096M -jar server.jar nogui
 
 set /p Build=<ver.txt
 set /a Build=%Build%+1
-echo %Build%>ver.txt
+>ver.txt echo %Build%
 
 git add .
 git commit -m "session #%Build%"
