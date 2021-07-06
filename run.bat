48dff25d75f4884f54bfe2ff0158ba98cfe3491b
James Lim <jameslimbj@gmail.com>
2021-07-06 15:59:55 +0800

    feat(git): add session id to commit message

--- run.bat
+++ run.bat
@@ -3,7 +3,9 @@ git stash
 git fetch --all
 git reset --hard origin/main
 java -Xmx4096M -Xms4096M -jar server.jar nogui
+set /p Build=<ver.txt
+set /a Build=%Build%+1
 git add .
-git commit -m "new ver"
+git commit -m "session #%Build%"
 git push
 pause
